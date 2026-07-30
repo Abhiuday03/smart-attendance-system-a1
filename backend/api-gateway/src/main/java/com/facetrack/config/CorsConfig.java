23bfc793db649fd5a7d16c32a8815662e553e192
Kamavaram Harish <harishkamavram0007@gmail.com>
2026-07-30 21:00:17 +0530

    Added JWT filteration

--- backend/api-gateway/src/main/java/com/facetrack/config/CorsConfig.java
+++ backend/api-gateway/src/main/java/com/facetrack/config/CorsConfig.java
@@ -11,27 +11,30 @@ import java.util.Arrays;
 @Configuration
 public class CorsConfig {
 
-    @Bean
-    public CorsWebFilter corsWebFilter() {
-        CorsConfiguration corsConfig = new CorsConfiguration();
-        
-        // 1. Allow  frontend
-//        corsConfig.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
-        corsConfig.setAllowedOrigins(Arrays.asList("*"));
-        
-        // 2. Allow all standard HTTP methods, including OPTIONS (Crucial for CORS pre-flight)
-        corsConfig.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-        
-        // 3. Allow all headers (like Authorization for your JWT token)
-        corsConfig.setAllowedHeaders(Arrays.asList("*"));
-        
-        // 4. Cache the CORS response so the browser doesn't ask every single time
-        corsConfig.setMaxAge(3600L);
+	@Bean
+	CorsWebFilter corsWebFilter() {
+		CorsConfiguration corsConfig = new CorsConfiguration();
 
-        // Apply this configuration to ALL routes (/**)
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", corsConfig);
+		// 1. Allow frontend
+		corsConfig.setAllowedOrigins(Arrays.asList("http://localhost:5173", "https://smart-attend-ai.netlify.app"));
 
-        return new CorsWebFilter(source);
-    }
+		// 2. Allow all standard HTTP methods, including OPTIONS (Crucial for CORS
+		// pre-flight)
+		corsConfig.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+
+		// 3. Allow all headers (like Authorization for your JWT token)
+		corsConfig.setAllowedHeaders(Arrays.asList("*"));
+
+		// 4. Cache the CORS response so the browser doesn't ask every single time
+		corsConfig.setMaxAge(3600L);
+
+		// 5. Accepts Cookies [ withCredentials: true ]
+		corsConfig.setAllowCredentials(true);
+
+		// Apply this configuration to ALL routes (/**)
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", corsConfig);
+
+		return new CorsWebFilter(source);
+	}
 }
--- backend/api-gateway/src/main/java/com/facetrack/config/GatewayConfig.java
+++ backend/api-gateway/src/main/java/com/facetrack/config/GatewayConfig.java
@@ -1,30 +1,32 @@
 package com.facetrack.config;
 
-//import com.example.demo.filter.AuthenticationFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.facetrack.filter.AuthenticationFilter;
+
 @Configuration
 public class GatewayConfig {
 
-//    @Autowired
-//    private AuthenticationFilter authFilter;
+    @Autowired
+    private AuthenticationFilter authFilter;
 
     @Bean
-    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+    RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
         return builder.routes()
-                // Route 1: Auth Service (No Filter)
+                // Route: Auth Service (No Filter)
                 .route("auth-service", r -> r
                         .path("/auth/**")
                         .uri("lb://AUTH-SERVICE"))
 
            
-//                .route("service-a", r -> r
-//                        .path("/api/v1/data/**")
-////                        .filters(f -> f.filter(authFilter.apply(new AuthenticationFilter.Config())))
-//                        .uri("lb://SERVICEA"))
+                .route("service-a", r -> r
+                        .path("/api/v1/data/**")
+                        .filters(f -> f.filter(authFilter.apply(new AuthenticationFilter.Config())))
+                        .uri("lb://SERVICEA"))
                 .build();
     }
 }
